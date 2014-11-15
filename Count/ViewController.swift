dbc5aa022d5584654ad26a4ccfdeba4d7ce39fae
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2014-11-15 16:14:37 +0900

    SwiftでCountアプリ

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label : UILabel!
+    var number : Int = 0
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label!.text = nil
+        number = 0
+        label.text = "\(number)"
     }
+        // Do any additional setup after loading the view, typically from a nib.
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func plus(){
+        number = number + 1
+        label.text = "\(number)"
     }
 
 
