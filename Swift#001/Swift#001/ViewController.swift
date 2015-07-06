1f9d6af536e74aecbc3c77d9c43200038b4a226e
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-07-06 23:05:21 +0900

    Initial Commit

---
+++ Swift#001/Swift#001/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift#001
+//
+//  Created by ia on 2015/07/06.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
