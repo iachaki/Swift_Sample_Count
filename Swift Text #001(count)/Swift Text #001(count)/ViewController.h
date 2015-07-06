b310434b15fbfb5a6225f7b4b5afacf0a47aff3f
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-07-06 20:56:47 +0900

    Initial Commit

---
+++ Swift Text #001(count)/Swift Text #001(count)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Swift Text #001(count)
+//
+//  Created by ia on 2015/07/06.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Swift Text #001(count)/Swift Text #001(count)/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Swift Text #001(count)
+//
+//  Created by ia on 2015/07/06.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
